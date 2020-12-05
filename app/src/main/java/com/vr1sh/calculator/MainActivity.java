84ac9d4ba6d2a2f6469663fa1e4d5c619d5dabff
Vrishank Mishra <42439148+vr1sh@users.noreply.github.com>
2020-12-05 16:51:53 +0530

    Update MainActivity.java

--- app/src/main/java/com/vr1sh/calculator/MainActivity.java
+++ app/src/main/java/com/vr1sh/calculator/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nopalyer.calculator;
+package com.vr1sh.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.faendir.rhino_android.RhinoAndroidHelper;
+import com.nopalyer.calculator.R;
+
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ast.Scope;
