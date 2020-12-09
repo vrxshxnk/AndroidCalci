a0da3336af23e4f8bd220ae2d66df8a74080e4f5
Vrishank Mishra <42439148+vr1sh@users.noreply.github.com>
2020-12-09 14:14:53 +0530

    Update ScientificActivity.java

--- app/src/main/java/com/vr1sh/calculator/ScientificActivity.java
+++ app/src/main/java/com/vr1sh/calculator/ScientificActivity.java
@@ -7,9 +7,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 public class ScientificActivity extends AppCompatActivity {
 
-    Button bZero, bSin, bCos, bTan, bCot, bSec, bCsc, bLog, bSquare, bRoot, bPi, bE, bPlus, bMinus,
+    Button b0, bSin, bCos, bTan, bCot, bSec, bCsc, bLog, bSquare, bRoot, bPi, bE, bPlus, bMinus,
             bMultiply, bDivision, bEqual, bClear, bDot, bBracket, bBack;
     TextView tvInput,tvOutput;
     String process;
@@ -21,7 +24,7 @@ public class ScientificActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        bZero = findViewById(R.id.btn0);
+        b0 = findViewById(R.id.btn0);
         bSin = findViewById(R.id.btnSin);
         bCos = findViewById(R.id.btnCos);
         bTan = findViewById(R.id.btnTan);
@@ -45,6 +48,81 @@ public class ScientificActivity extends AppCompatActivity {
         tvInput = findViewById(R.id.tvInput);
         tvOutput = findViewById(R.id.tvOutput);
 
+        bClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvInput.setText("");
+                tvOutput.setText("");
+            }
+        });
+
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = tvInput.getText().toString();
+                tvInput.setText(process + "0");
+            }
+        });
+
+        bMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = tvInput.getText().toString();
+                tvInput.setText(process + "-");
+            }
+        });
+
+        bMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = tvInput.getText().toString();
+                tvInput.setText(process + "×");
+            }
+        });
+
+        bDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = tvInput.getText().toString();
+                tvInput.setText(process + "÷");
+            }
+        });
+
+        bDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = tvInput.getText().toString();
+                tvInput.setText(process + ".");
+            }
+        });
+        
+        bEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = tvInput.getText().toString();
+
+                process = process.replaceAll("×","*");
+                process = process.replaceAll("%","/100");
+                process = process.replaceAll("÷","/");
+
+                Context rhino = Context.enter();
+
+                rhino.setOptimizationLevel(-1);
+
+                String finalResult = "";
+
+                try {
+                    Scriptable scriptable = rhino.initStandardObjects();
+                    finalResult = rhino.evaluateString(scriptable,process,"javascript",1,null).toString();
+                } catch (Exception e){
+                    finalResult="0";
+                }
+
+                tvOutput.setText(finalResult);
+            }
+        });
+
+
     }
-    
+
 }
